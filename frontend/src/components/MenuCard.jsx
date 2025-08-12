bff890d4123047d9f58621605adac10812cbfb94
Saroj_Dev <sarojtech0005@gmail.com>
2025-08-12 21:58:16 +0530

    Converted Menu UI from dummy to real

--- frontend/src/components/MenuCard.jsx
+++ frontend/src/components/MenuCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function MenuCard({ imageUrl, title, description, price }) {
+function MenuCard({ image, title, description, price }) {
   return (
     <div className="w-full max-w-[290px] mx-auto bg-white border border-gray-200 rounded-xl shadow-md overflow-hidden transition-transform transform hover:scale-105 hover:shadow-xl duration-300 ease-in-out">
       <div className="overflow-hidden">
         <img
-          src={imageUrl}
+          src={image}
           alt={title}
           className="w-full h-40 object-cover transition-transform duration-300 hover:scale-110"
         />
--- frontend/src/pages/MealsPage.jsx
+++ frontend/src/pages/MealsPage.jsx
@@ -1,46 +1,36 @@
 import React, { useState } from "react";
 import MenuCard from "../components/MenuCard";
+import axiosInstance from "../helpers/axiosInstance";
+import { useEffect } from "react";
 
 function MealsPage() {
-
   const [search, setSearch] = useState("");
+  const [meals, setMeals] = useState([]);
 
+  async function fetchMeals() {
+    try {
+      const res = await axiosInstance.get("/meals");
+      console.log(res);
+      console.log(res.data);
+      setMeals(res.data.data);
+    } catch (error) {
+      console.log(error);
+      throw new Error("Error fetching meals");
+    }
+  }
 
-  const meals = [
-    {
-      title: "Paneer Tikka",
-      description: "A delicious grilled Indian appetizer with marinated paneer and veggies.",
-      price: 299,
-      imageUrl: "https://spicecravings.com/wp-content/uploads/2020/10/Paneer-Tikka-Featured-1.jpg",
-    },
-    {
-      title: "Masala Dosa",
-      description: "Crispy fermented rice crepe with spiced potato filling.",
-      price: 195,
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQACh1yWbIwNiWJOZ-8lkt9oGkf5cdMK4DV8Q&s",
-    },
-    {
-      title: "Butter Chicken",
-      description: "Creamy tomato-based curry with tender chicken pieces.",
-      price: 399,
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9k3aQ9aF-SoTeZrZOdkv5FL9A_Pfw2ghUJQ&s",
-    },
-    {
-      title: "Veg Biryani",
-      description: "Fragrant rice dish with mixed vegetables and aromatic spices.",
-      price: 150,
-      imageUrl: "https://www.indianveggiedelight.com/wp-content/uploads/2020/04/veg-biryani-instant-pot.jpg",
-    },
-  ];
+  useEffect(() => {
+    fetchMeals();
+  }, []);
 
-  const filteredMeals = meals.filter((meal) => 
+  const filteredMeals = meals.filter((meal) =>
     meal.title.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
     <div className="px-4">
       <div className="my-6 flex justify-center">
-        <input 
+        <input
           type="text"
           placeholder="Search meals..."
           value={search}
@@ -49,21 +39,21 @@ function MealsPage() {
         />
       </div>
 
-    <div className="flex flex-wrap gap-4 justify-start items-start">
-      {filteredMeals.length > 0 ? (
-      filteredMeals.map((meal, idx) => (
-        <MenuCard
-          key={idx}
-          imageUrl={meal.imageUrl}
-          title={meal.title}
-          description={meal.description}
-          price={meal.price}
-        />
-      ))
-      ) : (
-        <p className="text-2xl text-gray-500">No meal found</p>
-      )}
-    </div>
+      <div className="flex flex-wrap gap-4 justify-start items-start">
+        {filteredMeals.length > 0 ? (
+          filteredMeals.map((meal, idx) => (
+            <MenuCard
+              key={idx}
+              image={meal.image}
+              title={meal.title}
+              description={meal.description}
+              price={meal.price}
+            />
+          ))
+        ) : (
+          <p className="text-2xl text-gray-500">No meal found</p>
+        )}
+      </div>
     </div>
   );
 }
